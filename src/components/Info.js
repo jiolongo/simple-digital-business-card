126f75b88e3d2855a45bbc9fc602067e5ebf993b
Jiolo Miles Ngo <jiolomilesngo001@gmail.com>
2022-08-17 14:52:18 +0800

    Initial Commit

---
+++ src/components/Info.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Info(){
+    return(
+        <>  
+            <div className="info-container">
+                <img src="../images/Formal.png" className="info-image"/>
+                <h2> Jiolo Miles Ngo</h2>
+                <h3>Frontend Developer</h3>
+                <h5>jiolomilesngo001@gmail.com</h5>
+            </div>
+        </>
+        
+    )
+}
+
+
